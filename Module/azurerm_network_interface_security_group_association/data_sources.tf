1088b9e989cdd3d54e9dd5bfd98ad9d928281cce
rahulpateriya <rahulpateriya287@gmai.com>
2025-12-18 23:51:15 +0530

    correct data block

--- Module/azurerm_network_interface_security_group_association/data_sources.tf
+++ Module/azurerm_network_interface_security_group_association/data_sources.tf
@@ -2,10 +2,10 @@
 data "azurerm_network_interface" "nic" {
   for_each = var.nsg-nic-assoc
     name                = each.value.name_nic
-    resource_group_name = each.value.name_rg
+    resource_group_name = each.value.resource_group_name
 }
 data "azurerm_network_security_group" "nsg" {
   for_each = var.nsg-nic-assoc
   name                = each.value.name_nsg
-  resource_group_name = each.value.name_rg
+  resource_group_name = each.value.resource_group_name
 }
