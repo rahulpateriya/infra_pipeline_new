5c6a2e8e6c61b6ad83d99cfc0dfdfbea8f58bc51
rahulpateriya <rahulpateriya287@gmail.com>
2025-12-18 22:43:22 +0530

    Update azurerm provider version to 4.56.0

--- Environment/Dev/provider.tf
+++ Environment/Dev/provider.tf
@@ -2,10 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.14.0"
+      version = "4.56.0"
     }
   }
+backend "azurerm" {}
 }
+
 
 provider "azurerm" {
   features {}
